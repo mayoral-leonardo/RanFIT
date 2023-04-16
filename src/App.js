023a7b503aac73a05313517d91d5905c2128dabf
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2023-04-15 23:12:28 -0300

    Base Topbar setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Dashboard from './views/global/Dashboard/Dashboard';
+import Dashboard from './views/Dashboard/Dashboard';
+import Topbar from './views/global/Topbar/Topbar';
 
 function App() {
   return (
     <div className="app">
       <main className="content">
+        <Topbar/>
         <Dashboard />
       </main>
     </div>
---
+++ src/views/global/Topbar/Topbar.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { Box, IconButton } from "@mui/material"
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined"
+
+export default function Topbar() {
+  return (
+    <Box
+      display="flex"
+      justifyContent="flex-end"
+      backgroundColor="#202A44"
+      p={2}
+    >
+      <Box
+        display="flex"
+        borderRadius="3px"
+      >
+        <IconButton onClick={() => {}}>
+          <PersonOutlinedIcon sx={{ color: "#FFFFFF" }}/>
+        </IconButton>
+      </Box>
+
+    </Box>
+  )
+}
