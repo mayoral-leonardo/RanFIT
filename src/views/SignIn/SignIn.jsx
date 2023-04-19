6e8c83655d05380baac23210e844e409403fd18e
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2023-04-19 00:46:13 -0300

    created signIn component and base structure

---
+++ src/views/SignIn/SignIn.jsx
@@ -0,0 +1,55 @@
+import React from "react"
+import { Box, Button, InputLabel, TextField } from "@mui/material"
+import Header from "../../components/Header/Header"
+
+export default function SignIn() {
+  const iconLogo = require('../../assets/img/icon-logo.png')
+  return (
+    <div className='signin_main_container'>
+      <Box
+        display="flex"
+        flexDirection="row"
+        width="60%"
+        height="80%"
+        bgcolor="#FFFFFF"
+      >
+        <div className='signin_logo_container'>
+          <img
+            src={iconLogo}
+            alt="Logo"
+            width="70%"
+            height="70%"
+          />
+        </div>
+
+        <div className='signin_form_container'>
+          <div className='signin_form_card'>
+            <Header
+              title="Bem-vindo(a) ao RanFIT"
+              subtitle="Acesse sua conta!"
+            />
+
+            <div className='signin_form'>
+              <InputLabel htmlFor="email-input">Email</InputLabel>
+              <TextField
+                className="signin_form_input"
+                id="email-input"
+                variant="outlined"
+                style={{ marginBottom: "5px" }}
+              />
+              <InputLabel htmlFor="password-input">Senha</InputLabel>
+              <TextField
+                className="signin_form_input"
+                id="password-input"
+                variant="outlined"
+                style={{ marginTop: "5px" }}
+              />
+            </div>
+            <Button variant="contained">Entrar</Button>
+          </div>
+        </div>
+
+      </Box>
+    </div>
+  )
+}
