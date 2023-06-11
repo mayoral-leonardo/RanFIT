59787fa94b2f0d16f55b5345967ccbb3b6779c43
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2023-06-11 17:08:26 -0300

    Applied hosting url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:'http://localhost:8082/api'
+  baseURL:'https://ranfitapi-production.up.railway.app/api'
 });
 
 export default api;
